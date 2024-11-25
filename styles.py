259e6b33f46fb18993ddb7f04bcf69da3dcbc49c
Gengery <kuznechik978@gmail.com>
2024-11-25 17:26:37 +0300

    Добавил немного текста, надо потестить выбор размера маркировки, работайте

--- styles.py
+++ styles.py
@@ -8,7 +8,8 @@ main_style = ("QPushButton { border: 1px solid black; border-radius: 7px; backgr
               "QPushButton:pressed { background-color: #abbaff; }"
               "QLineEdit { background-color: white; font-family: 'arial'; font-size: 18px; border: 1px solid black; border-radius: 7px; }"
               "QMainWindow { background-color: #ddeefb; }"
-              "QMenuBar { background-color: #ccdddd; }")
+              "QMenuBar { background-color: #ccdddd; }"
+              "QLabel { font-family: 'arial'; font-size: 18px; border-top: 2px solid black; }")
 
 dialog_style = ("QPushButton { border: 1px solid black; border-radius: 7px; background-color: #ccddff; font-family: 'arial'; font-size: 18px; padding: 8px; }"
               "QPushButton:hover { background-color: #bbbbff; }"
